800c7176cd8c034126a49ef172e5f7b4ea6800a6
kwezi <kwezi>
2019-06-12 16:39:29 +0200

    Few Fixes

--- app.js
+++ app.js
@@ -15,16 +15,4 @@ fs.readFile('./index.html', function (err, html) {
         res.end();
     }).listen(3000, '127.0.0.1'); 
     console.log('Started in server port 3000');
-});      
-// let app = http.createServer((req, res) =>
-// {
-//     //response type
-//     res.writeHead(200, {'Content-Type': 'application/json'});
-//     //send back response
-//     res.end('Hello Wforld!\n');
-// });
-
-
-//start server
-// app.listen(3000, '127.0.0.1');
-// console.log('Started in server port 3000');
+});
